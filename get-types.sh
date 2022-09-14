b7c202f64ddb5fd0aa813d9cf61651932a6c7278
Gino Filicetti <ginof@google.com>
2022-09-14 12:51:28 -0400

    Added script to print out the types

---
+++ get-types.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#project vars
+IMS_BUCKET=ims-assets-1
+PROJECT_ID=fox-ims-pilot
+LOCATION=us-west2
+
+#asset vars
+COMPLEXTYPE_ID=newsclip
+ASSETTYPE_ID=newsclipfile
+
+#authToken
+authToken=$(gcloud auth application-default print-access-token)
+
+#get ComplexType
+printf "=============================\n"
+printf "== GETTING $COMPLEXTYPE_ID ==\n" 
+printf "=============================\n"
+curl --silent -X GET \
+-H "Authorization: Bearer $authToken" \
+"https://mediaasset.googleapis.com/v1/projects/$PROJECT_ID/locations/$LOCATION/complexTypes/$COMPLEXTYPE_ID"
+
+#get AssetType
+printf "=============================\n"
+printf "== GETTING $ASSETTYPE_ID ==\n" 
+printf "=============================\n"
+curl --silent -X GET \
+-H "Authorization: Bearer $authToken" \
+"https://mediaasset.googleapis.com/v1/projects/$PROJECT_ID/locations/$LOCATION/assetTypes/$ASSETTYPE_ID"
