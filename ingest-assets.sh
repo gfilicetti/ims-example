dc563036c22d58476cbfe766b3f6a05fb4595e4c
Gino Filicetti <ginof@google.com>
2022-09-16 18:07:14 -0400

    update to removed too much debug, remove status checking (that should be done with the separate script) and changed ID creation so that it's always 4 characters. Improved the output to the screen.

--- ingest-assets.sh
+++ ingest-assets.sh
@@ -21,14 +21,21 @@ printf "== ADDING ASSETS TO INDEX ==\n"
 printf "============================\n"
 for curFile in $(gsutil ls -r gs://$IMS_BUCKET/$ARCHIVE/*)
 do 
+    # get only the path of the file, don't need bucket name
     STORAGE_INPUT_VIDEO=${curFile#gs://$IMS_BUCKET/$ARCHIVE/}
-    ASSET_ID=$(printf "%04g$i")
+
+    # create a sequential id that's always 4 digits wide
+    ASSET_ID=$(printf "%04d" $i)
+
+    # use the create time of the file in storage
     CREATE_TIME=$(gsutil ls -l gs://$IMS_BUCKET/$ARCHIVE/$STORAGE_INPUT_VIDEO | awk {'print $2'} | head -c -2)
 
     # if the current file is just a folder, then skip to the next file
     [[ ! $STORAGE_INPUT_VIDEO ]] && continue
 
-    printf "Processing file: ${curFile}\n"
+    printf "=================================\n"
+    printf "== Processing file: ${curFile} ==\n"
+    printf "=================================\n"
     printf "Ingesting with: \n"
     printf "\tassetID: ${ASSET_ID} \n"
     printf "\tobject: $ARCHIVE/$STORAGE_INPUT_VIDEO \n"
@@ -54,24 +61,17 @@ do
 
     ((i+=1))
 
-    printf "Making API request with JSON: \n$JSON_STRING\n"
+    # output the request JSON doc before making the call
+    # printf "Making API request with JSON: \n$JSON_STRING\n"
+
+    printf "== CALLING API ==\n"
 
     curl -X POST \
     -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
     -H "Content-Type: application/json; charset=utf-8" \
     -d  "$JSON_STRING" \
     "https://mediaasset.googleapis.com/v1/projects/$PROJECT_ID/locations/$LOCATION/assetTypes/$ASSETTYPE_ID/assets?asset_id=$ASSET_ID"
+
+    printf "== WAITING FOR 1s ==\n"
+
 done
-
-printf "===============================\n"
-printf "== CHECKING STATUS ON ASSETS ==\n"
-printf "===============================\n"
-
-for (( j = $i-1; j>0; j--)) 
-do
-    ASSET_ID=$(printf "%04g$j")
-
-    curl -X GET -H "Authorization: Bearer $authToken" \
-    "https://mediaasset.googleapis.com/v1/projects/$PROJECT_ID/locations/$LOCATION/assetTypes/$ASSETTYPE_ID/assets/$ASSET_ID/actions"
-done
-
