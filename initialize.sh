b7edc0c933a85057bfe8b90e1277d55f6be8276e
Gino Filicetti <ginof@google.com>
2022-09-16 16:47:42 -0400

    added the enablement of media APIs to the init script

--- initialize.sh
+++ initialize.sh
@@ -4,10 +4,14 @@
 #project vars
 PROJECT_ID=${1:-$(gcloud config get project)}
 
+printf "===========================\n"
+printf "== ENABLE THE MEDIA APIs ==\n"
+printf "===========================\n"
+gcloud services enable mediaasset.googleapis.com
+
 printf "=======================================\n"
 printf "== INITIALIZING PROJECT FOR IMS APIs ==\n"
 printf "=======================================\n"
-
 gcloud projects add-iam-policy-binding \
     --member=serviceAccount:cloud-control2-media-asset-clh@system.gserviceaccount.com \
     --role=roles/storage.objectViewer ${PROJECT_ID}
