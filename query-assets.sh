a1e688d7730487c49adb52a96bf7cfc8ae6fee40
Gino Filicetti <ginof@google.com>
2022-09-22 17:49:03 -0400

    enhanced query-assets.sh to get fileName through new query and colour code the segment times, plus more.

--- query-assets.sh
+++ query-assets.sh
@@ -27,7 +27,7 @@ GREEN=$'\e[1;32m'
 YELLOW=$'\e[1;33m'
 RESET=$'\e[0m'
 
-#conversion function for seconds
+#conversion function for seconds to mm:ss
 convertAndPrintSeconds() {
     local totalSeconds=$1;
     local seconds=$((totalSeconds%60));
@@ -53,12 +53,15 @@ printf "=====================\n"
 printf "$QUERY_JSON\n"
 
 queryResults=$(curl -s -X POST \
-    -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+    -H "Authorization: Bearer $authToken" \
     -H "Content-Type: application/json; charset=utf-8" \
     -d "$QUERY_JSON" \
     "https://mediaasset.googleapis.com/v1/projects/$PROJECT_ID/locations/$LOCATION/assetTypes/$ASSETTYPE_ID:search")
     
 # print raw results
+printf "===================\n"
+printf "== QUERY RESULTS ==\n"
+printf "===================\n"
 echo $queryResults | jq
 
 # use outer parens to put results into an array
@@ -72,15 +75,41 @@ resultLength=${#assetResults[@]}
 # loop through the number of results (in numbers)
 for i in $(seq $resultLength)
 do
-    # our loop is 1 based, we need 0 based for the array
-    curIndex=i-1
+    # our loop is 1 based, we need 0 based for the array so subtract 1 from the loop index
+    ((curIndex=i-1))
+
+    # to get the asset id, we need to take only the last element of the assetPath
+    # however, to do this in bash, which doesn't do non-greedy regex, we need to loop and cut
+    # off pieces of the assetPath until we're down to the last one
+    assetPath=${assetResults[$curIndex]}
+    assetId=$assetPath
+    regex='/(.*)$'
+    # loop and repeatedly cut the assetPath down using the '/' delimiter (see regex)
+    while [[ $assetId =~ $regex ]]; do
+        assetId=${BASH_REMATCH[1]}
+    done
+
+    # call using assetId to get asset information
+    assetResults=$(curl -s -X GET \
+        -H "Authorization: Bearer $authToken" \
+        "https://mediaasset.googleapis.com/v1/projects/$PROJECT_ID/locations/$LOCATION/assetTypes/$ASSETTYPE_ID/assets/$assetId")
+
+    assetFileName=$(echo $assetResults | jq -rc '.metadata.video_file.object')
+    assetBucket=$(echo $assetResults | jq -rc '.metadata.video_file.bucket')
 
     # lop off any fractional seconds, we don't need them
     startTime=${startSegmentResults[$curIndex]%"."*[0-9]"s"}
     endTime=${endSegmentResults[$curIndex]%"."*[0-9]"s"}
 
-    # print the asset id
-    printf "Asset: ${assetResults[$curIndex]}\n"
+    # print asset info
+    printf "==========\n"
+    printf "Asset Id: ${assetId}\n"
+    printf "==========\n"
+
+    # print bucket & filename
+    printf "Bucket: ${assetBucket}\n"
+    printf "Asset File: ${assetFileName}\n"
+    printf "==========\n"
 
     # convert start and end times from total seconds to mm:ss and output them
     printf "Segment: ${GREEN}$(convertAndPrintSeconds ${startTime})${RESET} --> ${RED}$(convertAndPrintSeconds $endTime)${RESET}\n\n"
