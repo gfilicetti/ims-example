bc79caf1bbf9a6744bc39f29bf4c72aff8a32de1
Gino Filicetti <ginof@google.com>
2023-01-06 12:43:08 -0500

    change code of get auth token to now properly work and prett-print using jq

--- get-auth-token-info.sh
+++ get-auth-token-info.sh
@@ -3,12 +3,11 @@
 # This will print out all the information about the app default access token 
 #   (including its expiry time in seconds)
 
-#authToken
+# get the authToken
 authToken=$(gcloud auth application-default print-access-token)
 
 printf "===================================\n"
 printf "== APP DEFAULT ACCESS TOKEN INFO ==\n" 
 printf "===================================\n"
-curl -H "Content-Type: application/x-www-form-urlencoded" \
--d "access_token=$authToken" \ 
-"https://www.googleapis.com/oauth2/v1/tokeninfo"
+results=$(curl -s -H "Content-Type: application/x-www-form-urlencoded" -d "access_token=$authToken" "https://www.googleapis.com/oauth2/v1/tokeninfo")
+echo $results | jq
